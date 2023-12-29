2529de212edd3f16d5aed56af9dfd1f903b1d86f
Johann Rojas <johann.rojasu@gmail.com>
2023-12-29 05:39:40 -0500

    feat(controller): add access control allow origin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,35 +1,40 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Header, Param, Patch, Post, Query } from '@nestjs/common'
 import { PaginationDto } from 'src/common/dto/pagination.dto'
+import { ParseMongoIdPipe } from 'src/common/parse-mongo-id/parse-mongo-id.pipe'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { UsersService } from './users.service'
-import { ParseMongoIdPipe } from 'src/common/parse-mongo-id/parse-mongo-id.pipe'
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
+  @Header('Access-Control-Allow-Origin', '*')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto)
   }
 
   @Get()
+  @Header('Access-Control-Allow-Origin', '*')
   findAll(@Query() paginationDto: PaginationDto) {
     return this.usersService.findAll(paginationDto)
   }
 
   @Get(':term')
+  @Header('Access-Control-Allow-Origin', '*')
   findOne(@Param('term') term: string) {
     return this.usersService.findOne(term)
   }
 
   @Patch(':term')
+  @Header('Access-Control-Allow-Origin', '*')
   update(@Param('term') term: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(term, updateUserDto)
   }
 
   @Delete(':id')
+  @Header('Access-Control-Allow-Origin', '*')
   remove(@Param('id', ParseMongoIdPipe) id: string) {
     return this.usersService.remove(id)
   }
